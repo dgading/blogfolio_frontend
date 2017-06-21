90a8e5830043db68723c9793892bcba96bd5d52c
Daniel Gading <dgading@gmail.com>
2017-06-21 12:52:46 -0400

    Add boundries to the fish tank

--- src/components/FishTank/Fish.js
+++ src/components/FishTank/Fish.js
@@ -3,31 +3,52 @@ import React from 'react';
 export class Fish extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {left: 100};
+    this.state = {
+      fish1: {
+        position: 0,
+        width: 200,
+        direction: 'rtl'
+      }
+    };
   }
 
   moveit() {
-    console.log(this.state.left)
-    this.setState({left: this.state.left + 50});
-  }
-
-  moveFish(currentPlace) {
-    setInterval(this.setState(this.moveit(currentPlace)), 5000)
+    let fishPosition = this.state.fish1.position;
+    let fishWidth = 100 - (this.state.fish1.width / 10);
+    let fishDirection = this.state.fish1.direction;
+    if(fishDirection === 'rtl') {
+      if(fishPosition < fishWidth) {
+        fishPosition = fishPosition + 5;
+        this.setState({fish1: {position: fishPosition, width: 200, direction: 'rtl'}});
+      } else {
+        fishPosition = fishPosition - 5;
+        this.setState({fish1: {position: fishPosition, width: 200, direction: 'ltr'}});
+      }
+    } else if(fishDirection === 'ltr') {
+      
+      if(fishPosition >= (10 - (this.state.fish1.width / 10))) {
+        fishPosition = fishPosition - 5;
+        this.setState({fish1: {position: fishPosition, width: 200, direction: 'ltr'}});
+      } else {
+        fishPosition = fishPosition + 5;
+        this.setState({fish1: {position: fishPosition, width: 200, direction: 'rtl'}});
+      }
+    }
   }
 
   componentDidMount() {
-    let currentPlace = this.state.left;
+    let currentPlace = this.state.fish1.position;
     let fishy = this.refs.fishy;
-    let intervalId = setInterval(this.moveit.bind(this), 2000);
+    let intervalId = setInterval(this.moveit.bind(this), 1000);
     this.setState({intervalId: intervalId});
   }
 
   render() {
     return(
       <span
-        style={{left: this.state.left + 'px'}}
+        style={{left: this.state.fish1.position + '%'}}
         ref="fishy"  
-        className="fish-tank__fish" 
+        className={"fish-tank__fish " + this.state.fish1.direction }
         dangerouslySetInnerHTML={{__html: this.props.fishType}} 
       />
     );
